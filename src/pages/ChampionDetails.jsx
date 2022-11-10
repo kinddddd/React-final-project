d7a257070bb824596456b52b9a7f37d84fa111ac
kinddddd <alvaromoreno21197@gmail.com>
2022-11-10 09:24:02 +0100

    BUG not fixed yet

--- src/pages/ChampionDetails.jsx
+++ src/pages/ChampionDetails.jsx
@@ -9,40 +9,41 @@ const ChampionDetails = () => {
     const navigate = useNavigate();
     const [champion, setChampion] = useState({});
     
+    
     useEffect(() => {
         const getChampion = async () => {
             const res = await axios.get(`http://ddragon.leagueoflegends.com/cdn/12.21.1/data/en_US/champion/${name}.json`)
             setChampion(res.data.data[name]);
-            console.log(res.data.data[name]);
         }
         getChampion();
     },[])
 
+    console.log(champion);
 
   return (
     <div className='champContainer'>
-        <div className='champ'>
-            <p className='title'>{champion.name}</p>
+        <div className='champBox'>
             <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg`} alt={champion.name} className="champImg"/>
-            <div className='lore'>
-                <p className='loreTitle'>Lore</p>
-                <p className='loreText'>{champion.lore}</p>
-            </div>
-            <div className='spells'>
-                <p className='spellsTitle'>Spells</p>
-                <div>
-                    {champion.spells.map((spell) => {
-                        return (
-                            <div className='spell'>
-                                <p>{spell.name}</p>
-                                <img src={`http://ddragon.leagueoflegends.com/cdn/12.21.1/img/spell/${spell.image.full}`} alt={spell.name}/>
-                                <p className='spellDescription'>{spell.description}</p>
-                            </div>
-                        )
-                    })}
+            <div className='champ'>
+                <div className='lore'>
+                    <p className='loreTitle'>{champion.name}</p>
+                    <p className='loreText'>{champion.lore}</p>
+                </div>
+                <div className='spells'>
+                    <p className='spellsTitle'>Spells</p>
+                    <div>
+                        {/* {champion.spells.map((spell) => {
+                            return (
+                                <div className='spell' key={spell.id}>
+                                    <p className='spellTitle'>{spell.name}</p>
+                                    <img src={`http://ddragon.leagueoflegends.com/cdn/12.21.1/img/spell/${spell.image.full}`} alt={spell.name}/>
+                                    <p className='spellDescription'>{spell.description}</p>
+                                </div>
+                            )
+                        })} */}
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
   )
